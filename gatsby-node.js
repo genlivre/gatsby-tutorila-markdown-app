93cb9073821ca378ee0fe1cb6bc006887d5e8b19
Genta <hutika.nico@gmail.com>
2021-02-18 09:00:37 +0900

    tutorial7 Markdownファイルを元に、ページを生成

---
+++ gatsby-node.js
@@ -0,0 +1,52 @@
+const path = require(`path`)
+const { createFilePath } = require(`gatsby-source-filesystem`)
+
+// Markdownファイルから、Slugの取得部分
+exports.onCreateNode = ({ node, getNode, actions }) => {
+  const { createNodeField } = actions
+  if (node.internal.type === `MarkdownRemark`) {
+    const slug = createFilePath({ node, getNode, basePath: `pages` })
+    createNodeField({
+      node,
+      name: `slug`,
+      value: slug,
+    })
+  }
+}
+
+// ページの作成部分
+/*
+  手順
+  1. GraphQLでデータをクエリする
+  2. クエリ結果をページにマップする
+*/
+
+exports.createPages = async ({ graphql, actions }) => {
+  const { createPage } = actions
+  const result = await graphql(`
+    query {
+      allMarkdownRemark {
+        edges {
+          node {
+            fields {
+              slug
+            }
+          }
+        }
+      }
+    }
+  `)
+
+  // テンプレートを使用してページを生成する
+  result.data.allMarkdownRemark.edges.forEach(({ node }) => {
+    createPage({
+      path: node.fields.slug,
+      component: path.resolve(`./src/templates/blog-post.js`),
+      context: {
+        // Data passed to context is available
+        // in page queries as GraphQL variables.
+        slug: node.fields.slug,
+      },
+    })
+  })
+}
