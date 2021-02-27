8dea06422931b22e71fda3eaad5ba84fae2fb44b
Genta <hutika.nico@gmail.com>
2021-02-27 15:14:43 +0900

    tutorial8 メタタグの追加とPWA対応

---
+++ src/pages/404.js
@@ -0,0 +1,24 @@
+import React from "react"
+import { graphql } from "gatsby"
+import Layout from "../components/layout"
+
+export default function About({ data }) {
+  return (
+    <Layout>
+      <h1>404 Not Found</h1>
+      <p>
+        ページが見つかりません
+      </p>
+    </Layout>
+  )
+}
+
+export const query = graphql`
+  query {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+  }
+`
